1416f96cb5121a8c6d9ce394de800de3995e5028
FabianMontañez <c4monta3z@gmail.com>
2020-10-05 19:57:53 -0500

    Distribucion de cuadros y se agrega animaciones a cada uno cuando pasa el mouse por encima de cada elemento

---
+++ index.js
@@ -0,0 +1,11 @@
+let myCardNumberSix = document.getElementById("cardNumberSix");
+
+myCardNumberSix.addEventListener('mouseover', () => {
+    // console.log(" Press in Card Number Six")
+    myCardNumberSix.classList="centered-container tricki-cell onhover-card";
+});
+
+function printText(){
+  console.log(" Press in Card Number Six")
+}
+
