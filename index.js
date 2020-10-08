bb16e3c5d8ea92234e2983570f9b828cc4c6b4e5
FabianMontañez <c4monta3z@gmail.com>
2020-10-07 19:58:08 -0500

    se trabaja en una estructura general

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-// import myEffectsUtils from './effets.js'
-// myEffectsUtils.setCardsPopEffect();
+function appendCardToParend(parent, number){
 
+  let myNewCard = document.createElement('div');
+      myNewCard
 
-
-
-
-function setCardsPopEffect(){
-    let myitems = document.getElementsByClassName('centered-container tricki-cell');
-      for(let i=0; i<myitems.length; i++){
-        let myCard = myitems[i];
-            myCard.addEventListener('mouseover', () => {
-              myCard.classList.add("onhover-card");
-            });
-          
-          myCard.addEventListener('mouseout', () => {
-            myCard.classList.remove('onhover-card');
-          });
-      } 
 }
-
-setCardsPopEffect();
