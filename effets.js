9d5accc1b852efdc5bd5ab8c04e652e022dcc48f
FabianMontañez <c4monta3z@gmail.com>
2020-10-07 19:48:15 -0500

    se agregan animaciones para cada tarjeta

--- effets.js
+++ effets.js
@@ -1,22 +1,20 @@
-// import myEffectsUtils from './effets.js'
-// myEffectsUtils.setCardsPopEffect();
-
-
-
-
 
 function setCardsPopEffect(){
+    
     let myitems = document.getElementsByClassName('centered-container tricki-cell');
-      for(let i=0; i<myitems.length; i++){
+      for(let i; i<myitems.length; i++){
         let myCard = myitems[i];
             myCard.addEventListener('mouseover', () => {
               myCard.classList.add("onhover-card");
             });
-          
+            
+  
           myCard.addEventListener('mouseout', () => {
             myCard.classList.remove('onhover-card');
           });
       } 
-}
-
-setCardsPopEffect();
+  }
+  
+  module.exports = {
+    setCardsPopEffect: setCardsPopEffect
+  }
