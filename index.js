c23f26a2e9a4eda2f4cde20874a693fa799deb24
FabianMontañez <c4monta3z@gmail.com>
2020-10-09 19:45:32 -0500

    Se Implementa imagenes orientado a juego

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function appendCardToParend(parent, number){
-
   let myNewCard = document.createElement('div');
-      myNewCard
+      myNewCard.className = "centered-container tricki-cell ";
 
+      myNewCard.addEventListener('mouseover', () => {
+        myNewCard.classList.add("onhover-card");
+      });
+
+      myNewCard.addEventListener('mouseout', () => {
+        myNewCard.classList.remove('onhover-card');
+      });
+
+      myNewCard.innerHTML = number;
+      parent.appendChild(myNewCard);
+
+}               
+
+function appendCardsRowToContainer(container){
+    let myNewCardsRow = document.createElement('div');
+    myNewCardsRow.className = "centered-container tricki-row";
+    container.appendChild(myNewCardsRow);
+    return myNewCardsRow;
 }
+
+let myCardsContainer = document.getElementById('mainCardsContainer');
+    for(let f=1;f<=1;f++){
+       let myNewRow = appendCardsRowToContainer(mainCardsContainer);
+            for(let e=1;e<=2; e++){
+                appendCardToParend(myNewRow, 3*(f-1)+e);
+            }
+
+    }
